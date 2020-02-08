76a23eb472bdb1734e5a86b87caec9b973bb4dd9
dandumitriu33 <dandumitriu33@gmail.com>
2020-02-08 18:04:59 +0200

    Add second task - 10x10 grid w/ color change

--- task-2/random_grid.js
+++ task-2/random_grid.js
@@ -1,5 +1,30 @@
 function main() {
-    // event handlers and other code here
+    let table10 = document.createElement("table");
+    table10.setAttribute('class', 'table10');
+    document.querySelector(".container").appendChild(table10);
+    for (let i=0; i<10; i++) {
+        let row = document.createElement("row");
+        row.setAttribute('class', `row${i}`);
+        document.querySelector(".table10").appendChild(row);
+        let currentRow;
+        for (let j=0; j<10; j++) {
+            let cell = document.createElement("td");
+            cell.setAttribute('class', 'cell');
+            cell.innerHTML = '<div class="active">[ ]</div>';
+            currentRow = 'row'+i;
+            document.querySelector(`.${currentRow}`).appendChild(cell);
+        }
+    }
+
+    function delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+    let allCells = document.querySelectorAll(".active");
+    let step = 500;
+    for (let i=0; i<100; i++) {        
+        delay(step).then(() => allCells[i].setAttribute('class', '.inactive'));
+        step += 500;
+    }
 }
 
 main();
